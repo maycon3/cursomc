892af492b9e2bfab879f05e686f6f01cab34bdab
Maycon <maycon.trb@gmail.com>
2018-05-03 18:23:35 -0300

    Banco de dados h2 criado com jpa

---
+++ src/main/java/com/maycon/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.maycon.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maycon.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method= RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);		
+		return categorias;
+	}
+
+}
