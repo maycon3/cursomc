1441ffc815c6317f847ceea3488cea863c038772
Maycon <maycon.trb@gmail.com>
2018-05-05 10:42:06 -0300

    Estado e Cidade

---
+++ src/main/java/com/maycon/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.maycon.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maycon.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+}
