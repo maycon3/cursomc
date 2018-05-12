afe49e8965474dd7fcb3ee024d9faf888f3840a8
Maycon <maycon.trb@gmail.com>
2018-05-12 10:07:49 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/maycon/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.maycon.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maycon.cursomc.domain.Cliente;
+import com.maycon.cursomc.exceptions.ObjectNotFoundException;
+import com.maycon.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto Não Encontrado Id: "+ id +", tipo :"+
+		Cliente.class.getName()));
+	}
+	
+	
+}
